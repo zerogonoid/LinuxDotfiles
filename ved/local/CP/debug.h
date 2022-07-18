c48ec19cecf354a1872a728c1bfecad51dca5cc9
zerogonoid <zerogonoid@gmail.com>
2022-07-18 18:20:31 +0530

    cleaned begm

--- ved/local/CP/debug.h
+++ ved/local/CP/debug.h
@@ -2,6 +2,8 @@
 #define _GLIBCXX_DEBUG
 using namespace std;
 
+const char* LAST_LINE = "\033[1;32m--------------------------\033[0m\n";
+
 void debug_print(string s) { cerr << "\"" << s << "\""; }
 void debug_print(const char* s) { debug_print((string)s); }
 void debug_print(bool val) { cerr << (val ? "true" : "false"); }
@@ -11,7 +13,7 @@ void debug_print(long long val) { cerr << val; }
 void debug_print(long double val) { cerr << val; }
 
 template<typename F, typename S>
-  void debug_print(pair<F, S> val) {
+void debug_print(pair<F, S> val) {
   cerr << "(";
   debug_print(val.first);
   cerr << ", ";
@@ -37,7 +39,7 @@ template<typename T>
 void debug_print(T val) {
   cerr << "{";
   bool first = true;
-  for (const auto &x : val) {
+  for (const auto& x : val) {
     if (!first) {
       cerr << ", ";
     } else {
@@ -46,17 +48,6 @@ void debug_print(T val) {
     debug_print(x);
   }
   cerr << "}";
-}
-
-void debug_print_collection() {
-  cerr << endl;
-}
-
-template<typename First, typename... Args>
-  void debug_print_collection(First val, Args... args) {
-  cerr << " ";
-  debug_print(val);
-  debug_print_collection(args...);
 }
 
 vector<string> vec_splitter(string s) {
@@ -69,26 +60,26 @@ vector<string> vec_splitter(string s) {
   return res;
 }
 void debug_out(
-               vector<string> __attribute__ ((unused)) args,
-               __attribute__ ((unused)) int idx,
-               __attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
+  vector<string> __attribute__ ((unused)) args,
+  __attribute__ ((unused)) int idx,
+  __attribute__ ((unused)) int LINE_NUM) { cerr << "\033[0m\n"; }
 template <typename Head, typename... Tail>
 void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
-  if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+  if(idx > 0) cerr << ", "; else cerr << "\033[2;36m" << "Line(" << LINE_NUM << ") ";
   cerr << args[idx] << " = ";
   debug_print(H);
   debug_out(args, idx + 1, LINE_NUM, T...);
 }
 
-void debug_(
-               vector<string> __attribute__ ((unused)) args,
-               __attribute__ ((unused)) int idx,
-               __attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
-template <typename Head, typename... Tail>
-void debug_(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
-  cerr << "\033[2;36m";
-  debug_out(args, idx, LINE_NUM, H, T...);
-  cerr << "\033[0m";
-}
+#define deb(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
 
-#define deb(...) debug_(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+
+void out(const int& type) {
+  static int casecount(0);
+  if (type == 0) {
+    cout << "\033[32m" << "--------------------------" << "Case#" << ++casecount << "\033[0m" <<  '\n';
+  } else if (type == 1) {
+    cout << "\033[1;32m" << "--------------------------" << "\033[0m" <<  '\n';
+    cerr << "finished in " << "\033[1;31m" << clock() * 1.0 / CLOCKS_PER_SEC << "\033[0m" << " sec" << endl;
+  }
+}
